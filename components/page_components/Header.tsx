258ddb3af9ff6a9439364102bfdecfc014528ac3
k1414k <k14146k@naver.com>
2025-09-16 05:04:57 +0900

    test

--- components/page_components/Header.tsx
+++ components/page_components/Header.tsx
@@ -1,13 +1,51 @@
+import Image from "next/image";
+import { Button } from "../ui/Button";
+import { Input } from "../ui/Input";
+
 export function Header(){
     return (
-      <header className="flex justify-between items-center px-[100px] py-5 border-b border-gray-100 ">
-        <div className="text-2xl font-bold text-rose-600">
-          AUCTION
+      <header className="grid grid-cols-10 items-center justify-end bg-gradient-to-l from-gray-700 to-gray-900">
+        <div className="col-span-1 col-start-2 text-2xl font-black text-purple-200 ">
+          <div className="cursor-pointer border-purple-200 border p-1 text-center rounded-3xl">
+            <div className="cursor-pointer bg-white text-purple-700 border-purple-200 border p-1 text-center rounded-3xl">
+              Auction ✴︎
+            </div>
+          </div>
         </div>
-        <nav className="space-x-4">
-          <a href="#" className="text-gray-700 hover:text-blue-500">求人</a>
-          <a href="#" className="text-gray-700 hover:text-blue-500">企業情報</a>
-          <a href="#" className="text-gray-700 hover:text-blue-500">ログイン</a>
+        <div className="col-span-5 col-start-3 ml-5 ">
+          <div className="grid grild-cols-10 items-center justify-center">
+            <Image src={'/search.png'} className="col-span-1 col-start-1" alt="asd" width={30} height={30}></Image>
+            <input
+              placeholder="ここを押して探しているモデル名入力. iphone, Macbook ..." 
+              className="
+              col-span-6 col-start-2
+              border border-gray-300 rounded-lg px-5 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            />
+            <span className="
+            col-span-2 col-start-8
+            inline-block">
+              検索
+            </span>
+          </div>
+        </div>
+        <nav className="col-span-3 col-start-8 space-x-4 py-3"> 
+          <div className="text-end mr-5">
+            <a href="#" className="text-white hover:text-blue-500">
+              <Button variant="secondary" size="md">
+                ログイン
+              </Button>
+            </a>
+            <a href="#" className="text-white hover:text-blue-500">
+              <Button variant="secondary" size="md">
+                ⭐️ リスト 0
+              </Button>
+            </a>
+            <a href="#" className="text-white hover:text-blue-500">
+              <Button variant="secondary" size="md">
+                三
+              </Button>
+            </a>
+          </div>
         </nav>
       </header>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,10 @@ import { Header } from "@/components/page_components/Header";
 
 export default function HomePage() {
   return (
-    // <div className="p-6 text-center space-y-6 mx-auto min-h-screen">
     <div>
       <Header />
-      {/* // className="max-w-6xl mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" */}
       <main className="max-w-[800px] mx-auto px-4 min-h-[100vh]">
-      <section className="rounded-md bg-gradient-to-r from-pink-100 to-rose-300 py-10 my-5 text-center">
+      {/* <section className="rounded-md bg-gradient-to-r from-pink-100 to-rose-300 py-10 my-5 text-center">
         <h1 className="text-4xl font-bold m-4 p-3">
           安く買う、高く売る、共存する
         </h1>
@@ -21,17 +19,17 @@ export default function HomePage() {
         <div className="px-10 mb-8">
           <Input className="text-blue-600" label="探しているモデル名入力" placeholder="iphone, macbook ..." />
         </div>
-      </section>
-        <Card title={"iphone"} subtitle={"スマホ"}>
+      </section> */}
+        {/* <Card title={"iphone"} subtitle={"スマホ"}>
           <Button variant="primary" size="lg">
             買う
           </Button>
           <Button variant="danger" size="lg">
             売る
           </Button>
-        </Card>
+        </Card> */}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
 
   );
