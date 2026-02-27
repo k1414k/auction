e26140ee7e083c1dc96058cd2993636818083aca
k1414k <k14146k@naver.com>
2026-02-28 05:10:04 +0900

    axios config 修正

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /* NEXT API を呼び出すときは lib/fetch　ファイルを参照する */
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // .env.local
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // .env
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
