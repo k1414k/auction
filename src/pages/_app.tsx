27688abf133c7d7ef683a9af44c09c3452fe72f9
k1414k <k14146k@naver.com>
2026-03-09 13:33:59 +0900

    ios UI 改善

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,8 +33,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserInitializer>
       <header className="sticky top-0 z-40 bg-white/90 backdrop-blur-sm border-b">
-        <div className="max-w-[1420px] mx-auto px-4 py-3 flex gap-3 items-center">
-          <LogoHeader />
+        <div className="max-w-[1420px] mx-auto px-4 py-3 flex gap-3 items-start">
+          <div className="shrink-0 pt-0.5">
+            <LogoHeader />
+          </div>
           <SearchHeader />
         </div>
         <PCNav />
